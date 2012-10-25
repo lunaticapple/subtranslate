4f5a6173f6bd018fb1a7b97be8497f249195407b
lunaticapple <lunaticapple@gmail.com>
2012-10-25 15:09:38 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SubtranslateTest::Application.config.secret_token = '845e547aa92dded020b5bc03634079accc9edd37d0d7a9b215b158bf727fc818a5f4cf9b1a4d09c47c7fbdb5052f95bf36910193283f9242064c90e85a891eef'
